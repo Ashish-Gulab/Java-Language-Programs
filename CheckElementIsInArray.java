7fefada915f3e9d79f16eeca8120a2064fcc758c
Ashish Gulab <94819616+Ashish-Gulab@users.noreply.github.com>
2022-11-02 10:45:50 +0530

    Update CheckElementIsInArray.java

--- CheckElementIsInArray.java
+++ CheckElementIsInArray.java
@@ -36,3 +36,18 @@ class checkElementIsInArray
         }
     }
 }
+
+
+/******************************** OUTPUT *************************************************************************************************************************/
+
+PS C:\Users\DELL\Documents\Java Programs> cd "c:\Users\DELL\Documents\Java Programs\" ; if ($?) { javac CheckElementIsInArray.java } ; if ($?) { java CheckElementIsInArray }
+Enter the number elements of the Array:
+5
+Enter the elements of the Array:
+6 8 6 3 4
+The Array is: 
+{6 8 6 3 4 }
+Enter the element to which we have to find that is it the part of the Array:
+3
+3 is present in the Array at the index 3
+PS C:\Users\DELL\Documents\Java Programs>
